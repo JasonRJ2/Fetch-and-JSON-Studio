4bb3f40af5369ed4f84814b636335b360f406f62
JasonRJ2 <jasonrjohnston2@gmail.com>
2022-08-11 20:45:31 -0500

    astronaut json

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        return response.json();
+    }).then(function(json){
+        console.log(json);
+        const container = document.getElementById("container")
+        let astronauts = "";
+        for (astronauts of json) {
+            astronauts += `
+            <div class="astronauts">
+                <div class="bio">
+                    <h3>${astronauts.firstName} ${astronauts.lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${astronauts.hoursInSpace}</li>
+                        <li>Active: ${astronauts.active}</li>
+                        <li>Skills: ${astronauts.skills.join(", ")}</li>
+                    </ul>
+                </div>
+            <img class="avatar" src="images/${astronauts.picture}"
+            </div>
+            `;
+        }
+        container.innerHTML = astronauts;
+    });
+});
